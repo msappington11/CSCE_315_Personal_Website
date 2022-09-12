b3fe98ba5219f7b89b1ebade36cce00881d09516
Mitchell Sappington <msappington11@gmail.com>
2022-09-12 10:29:17 -0500

    added persistent style change

--- javascript.js
+++ javascript.js
@@ -1,12 +1,26 @@
+window.onload = function() {
+    console.log('here')
+    console.log(localStorage.getItem("page_stylesheet_num"))
+    if(localStorage.getItem("page_stylesheet_num") === "2") {
+        let src = document.getElementById('style').href
+        let second_style = src.slice(0, -5) + "2" + src.slice(-4)
+        document.getElementById('style').href = second_style
+        console.log('second')
+    }
+}
+
 function changeTheme() {
     let src = document.getElementById('style').href
     let style = src.slice(-5,-4)
-    if(style === "1") {
-        document.getElementById('style').href = src.slice(0, -5) + "2" + src.slice(-4)
-        console.log(src.slice(0, -5) + "2" + src.slice(-6))
+    
+    if(localStorage.getItem("page_stylesheet_num") === "2") {
+        let first_style = src.slice(0, -5) + "1" + src.slice(-4)
+        document.getElementById('style').href = first_style
+        localStorage.setItem("page_stylesheet_num", "1")
     }
-    else if(style === "2") {
-        document.getElementById('style').href = src.slice(0, -5) + "1" + src.slice(-4)
-        console.log(src.slice(0, -5) + "1" + src.slice(-6))
+    else {
+        let second_style = src.slice(0, -5) + "2" + src.slice(-4)
+        document.getElementById('style').href = second_style
+        localStorage.setItem("page_stylesheet_num", "2");
     }
 }
